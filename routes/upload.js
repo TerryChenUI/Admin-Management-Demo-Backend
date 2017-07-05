405a5ac6a88cb01aea72cf77a6b4c842132b54ef
unknown <tchen@DSGDYN7Q52.prod.quest.corp>
2017-07-05 16:52:27 +0800

    update

--- routes/upload.js
+++ routes/upload.js
@@ -4,6 +4,23 @@ const { handleSuccess, handleError } = require('../utils/handle');
 const { uploadFile, deleteFile } = require('../utils/file');
 
 router
+  .post('/', async (ctx, next) => {
+    // try {
+      const uploadPath = path.join(__dirname, '..', 'public/upload');
+      const result = await uploadFile(ctx, {
+        fileType: 'content',
+        path: uploadPath
+      });
+      const data = {
+        link: result.filePath
+      }
+      handleSuccess({ ctx, result: data, message: '上传成功' });
+    // }
+    // catch (error) {
+    //   ctx.status = 500;
+    //   handleError({ ctx, error, message: '上传失败' });
+    // }
+  })
   .post('/thumb', async (ctx, next) => {
     try {
       const uploadPath = path.join(__dirname, '..', 'public/upload');
